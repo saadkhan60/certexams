f30be844c7b50b5c486b288ab68a21dd92ebbe4d
hmad <f23barin1e02112@iub.edu.pk>
2025-04-23 16:22:27 +0500

    meta data and ui fixes

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,7 +1,7 @@
 import { generateMetadata } from '@/lib/generate-metadata'
 import React from 'react'
 
-export const metadata = generateMetadata({title: "Contact Us | CertExams", description: "Contact CertExams for any inquiries or support regarding PMP certification preparation, exam dumps, and study materials. Our team is here to assist you with your PMP journey."})
+export const metadata = generateMetadata({title: "Contact Us", description: "Contact CertExams for any inquiries or support regarding PMP certification preparation, exam dumps, and study materials. Our team is here to assist you with your PMP journey."})
 
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
